ecf9bef55e7afd0d84e379cee00b1a39d6906acf
Chamindu Thiranjaya <chamindu.cta@gmail.com>
2018-06-20 21:26:37 +0530

    added generate schedule

--- src/app/TimeKeeper/generate-schedule/generate-schedule.component.ts
+++ src/app/TimeKeeper/generate-schedule/generate-schedule.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./generate-schedule.component.scss']
 })
 export class GenerateScheduleComponent implements OnInit {
-
+  routes = [
+    '120 - Colombo - Horana',
+    '120 - Colombo - Kesbewa',
+    '138 - Colombo - Kottawa',
+    '157 - Piliyandala - Madapatha'
+  ];
   constructor() { }
 
   ngOnInit() {
