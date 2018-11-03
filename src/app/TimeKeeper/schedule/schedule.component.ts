98aaba1ec0c66aced2a7c447201bf0812fa2ab43
Chamindu Thiranjaya <chamindu.cta@gmail.com>
2018-11-03 13:41:42 +0530

    date selection added for view schedule

--- src/app/TimeKeeper/schedule/schedule.component.ts
+++ src/app/TimeKeeper/schedule/schedule.component.ts
@@ -5,6 +5,12 @@ import { BlockUI, NgBlockUI } from 'ng-block-ui';
 import { Router } from '@angular/router'
 import * as jspdf from 'jspdf';
 import html2canvas from 'html2canvas';
+import swal from 'sweetalert2';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
@@ -14,19 +20,49 @@ export class ScheduleComponent implements OnInit {
   @BlockUI() blockUI: NgBlockUI;
   routes = [];
   allocation: any = []
-  constructor(private scheduleService: ScheduleServiceService, private routeService: RouteServiceService, private router: Router) { }
+  scheduleForm: FormGroup;
+  validTextType: boolean;
+  constructor(
+    private scheduleService: ScheduleServiceService,
+    private routeService: RouteServiceService,
+    private router: Router,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.scheduleForm = this.formBuilder.group({
+      route: [null, Validators.required],
+      date: [null, Validators.required],
+    })
     this.routeService.getRoutes().subscribe((data) => {
       this.routes = data.data;
     })
   }
 
-  onChange(event) {
+  textValidationType(e) {
+    e ? (this.validTextType = true) : (this.validTextType = false);
+  }
+
+  isFieldValid(form: FormGroup, field: string) {
+    return !form.get(field).valid && form.get(field).touched;
+  }
+
+  displayFieldCss(form: FormGroup, field: string) {
+    return {
+      'has-error': this.isFieldValid(form, field),
+      'has-feedback': this.isFieldValid(form, field)
+    };
+  }
+
+  viewSchedule() {
+
+    const formValues = this.scheduleForm.value
     this.blockUI.start('Loading...');
-    this.scheduleService.getSchedule({ route: event.value, date: new Date().setHours(0, 0, 0, 0) }).subscribe((data) => {
+    this.scheduleService.getSchedule({ route: formValues.route, date: new Date(formValues.date).setHours(0, 0, 0, 0) }).subscribe((data) => {
       this.blockUI.stop();
       this.allocation = data.data[0].allocation;
+    }, (error) => {
+      this.blockUI.stop();
+      this.showAlert('oops!', error.msg, 'error', 'btn btn-danger');
     })
   }
 
@@ -48,6 +84,22 @@ export class ScheduleComponent implements OnInit {
       var position = 0;
       pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
       pdf.save('MYPdf.pdf'); // Generated PDF   
+    });
+  }
+
+  /**
+   * show alert message
+   * @param title
+   * @param message
+   * @param type
+   */
+  showAlert(title, message, type, button) {
+    swal({
+      title: title,
+      text: message,
+      type: type,
+      confirmButtonClass: button,
+      buttonsStyling: false
     });
   }
 }
